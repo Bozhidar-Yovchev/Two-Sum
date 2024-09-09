a5290bc454c1d5772c8c20c6989eb0f139cdab5f
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-09 20:50:30 +0300

    array intiger index finder with specific target

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int target = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i]+nums[j]==target)
+                    {
+                        Console.WriteLine($"[{i},{j}]");
+                    }
+                }
+            }
+
+           
+
+        }
+    }
+}
